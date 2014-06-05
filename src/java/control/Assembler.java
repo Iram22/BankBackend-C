f904875df61022a7207787872829be3402462140
Iram <Iram@Iram.lan>
2014-06-05 14:09:07 +0200

    Final

--- src/java/control/Assembler.java
+++ src/java/control/Assembler.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class Assembler {
 
     public static CustomerSummary createCustomerSummary(Person customer) {
+        
         return new CustomerSummary(
                 customer.getCpr(),
                 customer.getFirstName() + " " + customer.getLastName(),
